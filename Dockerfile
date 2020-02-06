c6b5c4517d5f1783ddc6201c4486ccd03c1b9a24
Dan Cardin <ddcardin@gmail.com>
2020-02-06 17:54:19 -0500

    asdflkjasdf

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:slim-latest
+
+LABEL "com.github.actions.name"="release-manager"
+LABEL "com.github.actions.description"="Perform a release based on the PR comments."
+LABEL "com.github.actions.icon"="check-square"
+LABEL "com.github.actions.color"="yellow"
+
+
+# COPY requirements.txt
+# RUN pip install -r requirements.txt
+
+COPY "run.py"
+CMD [ "run.py" ]
---
+++ run.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+# import requests
+
+
+def run():
+    github_event_path = os.environ['GITHUB_EVENT_PATH']
+    gitbhub_token = os.environ['GITHUB_TOKEN']
+
+    with open(github_event_path, 'rb') as f:
+        github_event = json.loads(f)
+
+    print(github_event)
+
+    print()
+    print()
+
+    print(os.environ)
+
+
+if __name__ == '__main__':
+    run()
+
+# return unless github_event['action'] == 'created'
+#
+# body = File.read('/github/workspace/action-add-qa-message/qa_list.md')
+#
+# github_token = ENV['GITHUB_TOKEN']
+# api_version = 'v3'
+# header = {
+#   'Accept': "application/vnd.github.#{api_version}+json",
+#   'Authorization': "token #{github_token}"
+# }
+#
+# issue_number = github_event['issue']['number']
+# repo = ENV['GITHUB_REPOSITORY']
+#
+# uri = URI.parse("https://api.github.com/repos/#{repo}/issues/#{issue_number}/comments")
+#
+# http = Net::HTTP.new(uri.host, uri.port)
+# http.use_ssl = true
+# request = Net::HTTP::Post.new(uri.request_uri, header)
+# request.body = { body: body }.to_json
+#
+# http.request(request)
