923c4cb17c425aa95f5b5496a211d1a94eea680d
Dan Cardin <ddcardin@gmail.com>
2020-02-06 19:22:42 -0500

    asdf

--- run.py
+++ run.py
@@ -6,26 +6,41 @@ import os
 
 from github3 import login
 
+
 class BumpAmount(enum.Enum):
-    patch = 'patch'
-    minor = 'minor'
+    patch = "patch"
+    minor = "minor"
 
 
 class MergeMethod(enum.Enum):
-    merge = 'merge'
-    squash = 'squash'
-    rebase = 'rebase'
+    merge = "merge"
+    squash = "squash"
+    rebase = "rebase"
 
-parser = argparse.ArgumentParser(prog='merge', description='Process some integers.')
-parser.add_argument('--bump', type=BumpAmount, default=BumpAmount.minor
-                    help='an integer for the accumulator')
-parser.add_argument('-D', '--delete-branch', action='store_true',
-                    help='sum the integers (default: find the max)')
-parser.add_argument('--method', type=MergeMethod, default=MergeMethod.merge,
-                    help='sum the integers (default: find the max)')
+
+parser = argparse.ArgumentParser(prog="merge", description="Process some integers.")
+parser.add_argument(
+    "--bump",
+    type=BumpAmount,
+    default=BumpAmount.minor,
+    help="an integer for the accumulator",
+)
+parser.add_argument(
+    "-D",
+    "--delete-branch",
+    action="store_true",
+    help="sum the integers (default: find the max)",
+)
+parser.add_argument(
+    "--method",
+    type=MergeMethod,
+    default=MergeMethod.merge,
+    help="sum the integers (default: find the max)",
+)
+
 
 def parse_command(command):
-    if not command.startswith('/merge '):
+    if not command.startswith("/merge "):
         return
 
     split_command = shlex.split(command)
@@ -33,17 +48,18 @@ def parse_command(command):
 
     args = parser.parse_args(split_command)
 
-def run():
-    github_event_path = os.environ['GITHUB_EVENT_PATH']
-    github_token = os.environ['INPUT_REPO-TOKEN']
-    repository = os.environ['GITHUB_REPOSITORY']
 
-    with open(github_event_path, 'rb') as f:
+def run():
+    github_event_path = os.environ["GITHUB_EVENT_PATH"]
+    github_token = os.environ["INPUT_REPO-TOKEN"]
+    repository = os.environ["GITHUB_REPOSITORY"]
+
+    with open(github_event_path, "rb") as f:
         github_event = json.load(f)
 
-    user, repo = repository.split('/')
-    issue_number = github_event['issue']['number']
-    comment_body = github_event['comment']['body']
+    user, repo = repository.split("/")
+    issue_number = github_event["issue"]["number"]
+    comment_body = github_event["comment"]["body"]
 
     command = parse_command(comment_body)
 
@@ -61,28 +77,5 @@ def run():
     print(os.environ)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
-
-# eturn unless github_event['action'] == 'created'
-#
-# body = File.read('/github/workspace/action-add-qa-message/qa_list.md')
-#
-# github_token = ENV['GITHUB_TOKEN']
-# api_version = 'v3'
-# header = {
-#   'Accept': "application/vnd.github.#{api_version}+json",
-#   'Authorization': "token #{github_token}"
-# }
-#
-# issue_number = github_event['issue']['number']
-# repo = ENV['GITHUB_REPOSITORY']
-#
-# uri = URI.parse("https://api.github.com/repos/#{repo}/issues/#{issue_number}/comments")
-#
-# http = Net::HTTP.new(uri.host, uri.port)
-# http.use_ssl = true
-# request = Net::HTTP::Post.new(uri.request_uri, header)
-# request.body = { body: body }.to_json
-#
-# http.request(request)
